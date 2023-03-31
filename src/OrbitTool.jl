991c64bf9ed8ed4a3a0c4f15ef6d95e7f9fe8793
MichaszJ <mjski@live.ca>
2023-03-31 04:46:19 -0400

    added interactivity for num steps, orbit time

--- src/OrbitTool.jl
+++ src/OrbitTool.jl
@@ -1,9 +1,12 @@
+include("../src/SAT.jl");
+
 using GLMakie, GeoMakie, FileIO, .SAT
 using Downloads: download
 GLMakie.activate!()
 
 Makie.set_theme!(theme_dark())
 
+# source: https://beautiful.makie.org/dev/examples/generated/2d/geo/blue_marble/
 earth_img = load(download("https://upload.wikimedia.org/wikipedia/commons/5/56/Blue_Marble_Next_Generation_%2B_topography_%2B_bathymetry.jpg"));
 n = 1024 ÷ 4 # 2048
 θ = LinRange(0, π, n);
@@ -12,8 +15,8 @@ x = [cos(φ) * sin(θ) for θ in θ, φ in φ];
 y = [sin(φ) * sin(θ) for θ in θ, φ in φ];
 z = [cos(θ) for θ in θ, φ in φ];
 
-fig = Figure()
-ax1 = Axis3(fig[1,1], aspect=:data, viewmode=:fitzoom)
+fig = Figure(resolution=(2000,1000)); display(fig);
+ax1 = Axis3(fig[1,1], aspect=:data, viewmode=:fitzoom, title="Satellite ECI Frame Orbit")
 surf = surface!(
     ax1, x, y, z;
     color = earth_img,
@@ -31,10 +34,7 @@ ax2 = GeoAxis(
     title="Satellite Groundtrack",
     coastlines = true,
     coastline_attributes = (; color = "white"),
-)
-
-# orbital_elements, r_apo, r_per, t_init, t_final;
-# a, e, i, asc, arg, theta = orbital_elements
+);
 
 elements = Observable([
     26600,
@@ -45,17 +45,28 @@ elements = Observable([
     deg2rad(230)
 ]);
 
-sl = Slider(fig[2, 2], range = 50:1000, startvalue = 100, horizontal = true)
+ax3 = fig[2,1] = GridLayout()
+rowsize!(fig.layout, 2, Relative(1/5))
 
-const eq_rad = 6378.137 
-const ecc_sq = 6.69437999014e-3
+# tb_num_steps = Textbox(ax3[1,2], stored_string="100", validator = Int, textcolor=:white)
+num_steps_obsv = Observable(100)
+num_steps_sl = Slider(ax3[1,2], range = 50:1000, startvalue = to_value(num_steps_obsv), horizontal = true)
+connect!(num_steps_obsv, num_steps_sl.value)
+Label(ax3[1,1], @lift("Number of Computed Points: $($num_steps_obsv)"))
+
+final_time_obsv = Observable(24) # in hours
+final_time_sl = Slider(ax3[2,2], range = 1:50, startvalue = to_value(final_time_obsv), horizontal = true)
+connect!(final_time_obsv, final_time_sl.value)
+Label(ax3[2,1], @lift("Orbit Time: $($final_time_obsv) hr"))
 
 clamp_lon(lon) = lon % 180 - sign(lon)*180
 
 # initial plot
-lon, lat, alt, times = ground_track(elements, 0.0,  1*718*60.0, num_steps=100, return_times=true);
+lon, lat, alt, times = ground_track(to_value(elements), 0.0, to_value(final_time_obsv)*60^2, num_steps=to_value(num_steps_obsv), return_times=true);
 ground_track_coords = Observable(Matrix(hcat(clamp_lon.(rad2deg.(Float64.(lon))), rad2deg.(Float64.(lat)))'));
 
+const eq_rad = 6378.137 
+const ecc_sq = 6.69437999014e-3
 N = eq_rad ./ sqrt.(1 .- ecc_sq .* sin.(lat).^2);
 
 pos_x = (N .+ alt) .* cos.(lat) .* cos.(lon) ./ eq_rad;
@@ -67,9 +78,8 @@ pos_coords = Observable(Matrix(hcat(pos_x, pos_y, pos_z)'));
 scatter!(ax2, ground_track_coords, marker=:cross, color=:orange)
 lines!(ax1, pos_coords, linestyle = :dash, color=:orange)
 
-
-on(sl.value) do ns
-    lon, lat, alt, times = ground_track(elements, 0.0, 2.5*718*60.0, num_steps=ns, return_times=true)
+on(num_steps_sl.value) do ns
+    lon, lat, alt, times = ground_track(to_value(elements), 0.0, to_value(final_time_obsv)*60^2, num_steps=ns, return_times=true)
     ground_track_coords[] = Matrix(hcat(clamp_lon.(rad2deg.(Float64.(lon))), rad2deg.(Float64.(lat)))');
 
     N = eq_rad ./ sqrt.(1 .- ecc_sq .* sin.(lat).^2)
@@ -79,8 +89,19 @@ on(sl.value) do ns
     pos_z = ((1 - ecc_sq) .* N .+ alt) .* sin.(lat) ./ eq_rad
 
     pos_coords[] = Matrix(hcat(pos_x, pos_y, pos_z)')
+end
 
-    # Observable(Matrix(hcat(rad2deg.(Float64.(lon)), rad2deg.(Float64.(lat)))')), Observable(Float64.(pos_x)), Observable(Float64.(pos_y)), Observable(Float64.(pos_z))
+on(final_time_sl.value) do tf
+    lon, lat, alt, times = ground_track(to_value(elements), 0.0, tf*60^2, num_steps=to_value(num_steps_obsv), return_times=true)
+    ground_track_coords[] = Matrix(hcat(clamp_lon.(rad2deg.(Float64.(lon))), rad2deg.(Float64.(lat)))');
+
+    N = eq_rad ./ sqrt.(1 .- ecc_sq .* sin.(lat).^2)
+
+    pos_x = (N .+ alt) .* cos.(lat) .* cos.(lon) ./ eq_rad
+    pos_y = (N .+ alt) .* cos.(lat) .* sin.(lon) ./ eq_rad
+    pos_z = ((1 - ecc_sq) .* N .+ alt) .* sin.(lat) ./ eq_rad
+
+    pos_coords[] = Matrix(hcat(pos_x, pos_y, pos_z)')
 end
 
 
@@ -96,3 +117,15 @@ end
 #     # do something with `val`
 # end
 
+# sg = SliderGrid(
+#     fig[1, 2],
+#     (label = "Voltage", range = 0:0.1:10, format = "{:.1f}V", startvalue = 5.3),
+#     (label = "Current", range = 0:0.1:20, format = "{:.1f}A", startvalue = 10.2),
+#     (label = "Resistance", range = 0:0.1:30, format = "{:.1f}Ω", startvalue = 15.9),
+#     width = 350,
+#     tellheight = false)
+
+# sliderobservables = [s.value for s in sg.sliders]
+# bars = lift(sliderobservables...) do slvalues...
+#     [slvalues...]
+# end
