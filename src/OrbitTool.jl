24f3e066e24ebda8be1548fa9561d061f4923b3d
MichaszJ <mjski@live.ca>
2023-04-01 02:52:56 -0400

    implemented ground track projection interactivity

--- src/OrbitTool.jl
+++ src/OrbitTool.jl
@@ -15,6 +15,7 @@ x = [cos(φ) * sin(θ) for θ in θ, φ in φ];
 y = [sin(φ) * sin(θ) for θ in θ, φ in φ];
 z = [cos(θ) for θ in θ, φ in φ];
 
+begin
 fig = Figure(resolution=(2000,1000)); display(fig);
 ax1 = Axis3(fig[1,1], aspect=:data, viewmode=:fitzoom, title="Satellite ECI Frame Orbit")
 surf = surface!(
@@ -28,13 +29,6 @@ surf = surface!(
 Makie.rotate!(surf, Quaternionf(0.0, 0.0, 1, 0));
 
 colsize!(fig.layout, 1, Relative(1/3))
-
-ax2 = GeoAxis(
-    fig[1, 2]; 
-    title="Satellite Groundtrack",
-    coastlines = true,
-    coastline_attributes = (; color = "white"),
-);
 
 elements = Observable([
     26600,
@@ -59,11 +53,85 @@ final_time_sl = Slider(ax3[2,2], range = 1:50, startvalue = to_value(final_time_
 connect!(final_time_obsv, final_time_sl.value)
 Label(ax3[2,1], @lift("Orbit Time: $($final_time_obsv) hr"))
 
-clamp_lon(lon) = lon % 180 - sign(lon)*180
+# projections = ["+proj=adams_hemi", "+proj=adams_ws1", "+proj=adams_ws2",
+#     "+proj=aea +lat_1=29.5 +lat_2=42.5", "+proj=aeqd", "+proj=airy", "+proj=aitoff",
+#     "+proj=apian", "+proj=august", "+proj=bacon", "+proj=bertin1953", "+proj=bipc +ns",
+#     "+proj=boggs", "+proj=bonne +lat_1=10", "+proj=cass", "+proj=cea",
+#     "+proj=chamb +lat_1=10 +lon_1=30 +lon_2=40", "+proj=collg", "+proj=comill",
+#     "+proj=crast", "+proj=denoy", "+proj=eck1", "+proj=eck2", "+proj=eck3",
+#     "+proj=eck4", "+proj=eck5", "+proj=eck6", "+proj=eqc", "+proj=eqdc +lat_1=55 +lat_2=60",
+#     "+proj=eqearth", "+proj=euler +lat_1=67 +lat_2=75", "+proj=fahey", "+proj=fouc", "+proj=fouc_s",
+#     "+proj=gall", "+proj=geos +h=35785831.0 +lon_0=-60 +sweep=y", "+proj=gins8", "+proj=gn_sinu +m=2 +n=3",
+#     "+proj=goode", "+proj=guyou", "+proj=hammer", "+proj=hatano",
+#     "+proj=igh", "+proj=igh_o +lon_0=-160", "+proj=imw_p +lat_1=30 +lat_2=-40", "+proj=isea",
+#     "+proj=kav5", "+proj=kav7", "+proj=laea", "+proj=lagrng", "+proj=larr", "+proj=lask",
+#     "+proj=lcca +lat_0=35", "+proj=leac", "+proj=loxim",
+#     "+proj=lsat +ellps=GRS80 +lat_1=-60 +lat_2=60 +lsat=2 +path=2", "+proj=mbt_s", "+proj=mbt_fps",
+#     "+proj=mbtfpp", "+proj=mbtfpq", "+proj=mbtfps", "+proj=merc", "+proj=mill", "+proj=misrsom +path=1",
+#     "+proj=moll", "+proj=murd1 +lat_1=30 +lat_2=50",
+#     "+proj=murd3 +lat_1=30 +lat_2=50", "+proj=natearth", "+proj=natearth2",
+#     "+proj=nell", "+proj=nell_h", "+proj=nicol",
+#     "+proj=ob_tran +o_proj=mill +o_lon_p=40 +o_lat_p=50 +lon_0=60", "+proj=ocea", "+proj=oea +m=1 +n=2",
+#     "+proj=omerc +lat_1=45 +lat_2=55", "+proj=ortel", "+proj=ortho", "+proj=patterson", "+proj=poly",
+#     "+proj=putp1", "+proj=putp2", "+proj=putp3", "+proj=putp3p", "+proj=putp4p", "+proj=putp5",
+#     "+proj=putp5p", "+proj=putp6", "+proj=putp6p", "+proj=qua_aut", "+proj=robin", "+proj=rouss",
+#     "+proj=rpoly", "+proj=sinu", "+proj=times", "+proj=tissot +lat_1=60 +lat_2=65", "+proj=tmerc",
+#     "+proj=tobmerc", "+proj=tpeqd +lat_1=60 +lat_2=65", "+proj=urm5 +n=0.9 +alpha=2 +q=4",
+#     "+proj=urmfps +n=0.5", "+proj=vandg", "+proj=vandg2", "+proj=vandg3", "+proj=vandg4",
+#     "+proj=vitk1 +lat_1=45 +lat_2=55", "+proj=wag1", "+proj=wag2", "+proj=wag3", "+proj=wag4",
+#     "+proj=wag5", "+proj=wag6", "+proj=wag7", "+proj=webmerc +datum=WGS84", "+proj=weren",
+#     "+proj=wink1", "+proj=wink2", "+proj=wintri"
+# ]
+
+# projections = ["+proj=weren", "+proj=crast",
+#     "+proj=wink1", "+proj=wink2", "+proj=wintri", "+proj=poly",
+#     "+proj=putp1", "+proj=putp2", "+proj=putp3", "+proj=putp3p", "+proj=putp4p", "+proj=putp5",
+#     "+proj=putp5p", "+proj=putp6", "+proj=putp6p", "+proj=qua_aut", "+proj=robin", "+proj=rouss",
+#     "+proj=rpoly", "+proj=sinu"
+# ]
+
+projections = [
+    "+proj=natearth", "+proj=natearth2", "+proj=moll", "+proj=weren", "+proj=times", "+proj=wink1", "+proj=wink2","+proj=sinu", "+proj=crast",
+]
+
+proj_obsv = Observable("")
+Label(ax3[1,3], "Map Projection")
+proj_menu = Menu(ax3[2,3], options=projections)
+connect!(proj_obsv, proj_menu.selection)
+
+on(proj_menu.selection) do s
+    proj_obsv[] = s
+    # autolimits!(ax2)
+end
+
+proj = lift(proj_obsv) do s
+    s
+end
+
+ax2 = GeoAxis(
+    fig[1, 2]; 
+    title="Satellite Groundtrack",
+    coastlines = true,
+    coastline_attributes = (; color = "white"),
+    dest=proj,
+);
+
+# img = rotr90(GeoMakie.earth());
+# image!(ax2, -180..180, -90..90, img; interpolate = true) # this must be included
+# surface!(ax2,  -180..180, -90..90, ones(size(img)...); color = img, shading = false)
+
+clamp_lon(lon) = lon > 180.0 || lon < -180.0 ? lon % 180 - sign(lon)*floor(abs(lon/180))*180 : lon
+clamp_lat(lat) = lat > 90.0 || lat < -90.0 ? lat % 90 - sign(lat)*floor(abs(lat/90))*90 : lat
 
 # initial plot
 lon, lat, alt, times = ground_track(to_value(elements), 0.0, to_value(final_time_obsv)*60^2, num_steps=to_value(num_steps_obsv), return_times=true);
-ground_track_coords = Observable(Matrix(hcat(clamp_lon.(rad2deg.(Float64.(lon))), rad2deg.(Float64.(lat)))'));
+
+println("Lat: $(rad2deg(maximum(lat))) $(rad2deg(minimum(lat)))")
+println("Lat Clamp: $(clamp_lat(rad2deg(maximum(lat)))) $(clamp_lat(rad2deg(minimum(lat))))")
+println("Lon: $(rad2deg(maximum(lon))) $(rad2deg(minimum(lon)))")
+println("Lon Clamp: $(clamp_lon(rad2deg(maximum(lon)))) $(clamp_lon(rad2deg(minimum(lon))))")
+
+ground_track_coords = Observable(Matrix(hcat(clamp_lon.(rad2deg.(Float64.(lon))), clamp_lat.(rad2deg.(Float64.(lat))))'));
 
 const eq_rad = 6378.137 
 const ecc_sq = 6.69437999014e-3
@@ -80,7 +148,7 @@ lines!(ax1, pos_coords, linestyle = :dash, color=:orange)
 
 on(num_steps_sl.value) do ns
     lon, lat, alt, times = ground_track(to_value(elements), 0.0, to_value(final_time_obsv)*60^2, num_steps=ns, return_times=true)
-    ground_track_coords[] = Matrix(hcat(clamp_lon.(rad2deg.(Float64.(lon))), rad2deg.(Float64.(lat)))');
+    ground_track_coords[] = Matrix(hcat(clamp_lon.(rad2deg.(Float64.(lon))), clamp_lat.(rad2deg.(Float64.(lat))))')
 
     N = eq_rad ./ sqrt.(1 .- ecc_sq .* sin.(lat).^2)
 
@@ -93,7 +161,7 @@ end
 
 on(final_time_sl.value) do tf
     lon, lat, alt, times = ground_track(to_value(elements), 0.0, tf*60^2, num_steps=to_value(num_steps_obsv), return_times=true)
-    ground_track_coords[] = Matrix(hcat(clamp_lon.(rad2deg.(Float64.(lon))), rad2deg.(Float64.(lat)))');
+    ground_track_coords[] = Matrix(hcat(clamp_lon.(rad2deg.(Float64.(lon))), clamp_lat.(rad2deg.(Float64.(lat))))')
 
     N = eq_rad ./ sqrt.(1 .- ecc_sq .* sin.(lat).^2)
 
@@ -104,6 +172,7 @@ on(final_time_sl.value) do tf
     pos_coords[] = Matrix(hcat(pos_x, pos_y, pos_z)')
 end
 
+end
 
 
 # sg = SliderGrid(fig[1, 1],
